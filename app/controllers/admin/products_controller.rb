d061045713b6af71278e01daa00e8894fa9dbfb6
Oleg <gafrom@gmail.com>
2016-11-04 23:24:40 +0300

    Enable file uploads

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,47 +1,40 @@
 class Admin::ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
-  # GET /admin/products
-  # GET /admin/products.json
   def index
     @products = Product.all
   end
 
-  # GET /admin/products/1
-  # GET /admin/products/1.json
   def show
   end
 
-  # GET /admin/products/new
   def new
     @product = Product.new
   end
 
-  # GET /admin/products/1/edit
   def edit
   end
 
-  # POST /admin/products
   def create
-    @product = Product.new(product_params)
+    @product = Product.new product_params
 
-    if @product.save
-      redirect_to @product, notice: 'Product was successfully created.'
+    if @product.load_image && @product.save
+      redirect_to [:admin, @product], notice: 'Product was successfully created.'
     else
       render :new
     end
   end
 
-  # PATCH/PUT /admin/products/1
   def update
-    if @product.update(product_params)
-      redirect_to @product, notice: 'Product was successfully updated.'
+    @product.assign_attributes product_params
+
+    if @product.load_image && @product.save
+      redirect_to [:admin, @product], notice: 'Product was successfully updated.'
     else
       render :edit
     end
   end
 
-  # DELETE /admin/products/1
   def destroy
     @product.destroy
 
@@ -49,15 +42,13 @@ class Admin::ProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
       params.require(:product)
-            .permit(:name, :slug, :image, :description, :weight, 
-                    :product_type, :category_id, :producer_id)
+            .permit(:name, :slug, :description, :weight, :product_type,
+                    :category_id, :producer_id, :image)
     end
 end
--- app/models/imagable.rb
+++ app/models/imagable.rb
@@ -1,5 +1,36 @@
 module Imagable
+  ROOT_DIR = Rails.root.join('public', 'uploads', 'images')
+
   def image_url
-    "storage/images/#{self.class.name.downcase.pluralize}/#{image}"
+    "/uploads/images/#{folder}/#{image}"
+  end
+
+  def load_image
+    return true if image.nil?
+
+    uploaded_io = image_before_type_cast
+    filename = uploaded_io.original_filename
+
+    directory = ROOT_DIR.join folder
+    FileUtils.mkdir_p(directory) unless File.directory?(directory)
+
+    File.open(directory.join(filename), 'wb') do |file|
+      file.write(uploaded_io.read)
+    end
+
+    self.image = filename
+
+    true
+  rescue
+    msg = 'load error'
+    self.errors[:image] << msg
+
+    false
+  end
+
+  private
+
+  def folder
+    self.class.name.downcase.pluralize
   end
 end
